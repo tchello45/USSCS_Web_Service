dbb223e353e4d3956faf20c9ce2f62d922fba703
tchello45 <tilman.kurmayer@outlook.de>
2023-07-08 12:06:03 +0200

    setup

--- install_usscs.sh
+++ install_usscs.sh
@@ -1,5 +1,8 @@
 rm -rf USSCS
 git clone https://github.com/tchello45/USSCS.git
-cd USSCS
-sh setup.sh
+cd USSCS/usscs
+python3 setup.py install
+cd ..
+cd usscs_enc
+python3 setup.py install
 rm -rf USSCS
